118ae7fb98cdb483646cb1971b75dae2ee23e087
Cao Thanh Danh <caothanhdanh.developer.1602@gmail.com>
2022-10-21 16:45:15 +0700

    feature/student-crud-ThanhDanh Task 1 Fixbug Icon

--- bktel/public/js/app.js
+++ bktel/public/js/app.js
@@ -5353,7 +5353,7 @@ var staticRenderFns = [function () {
     staticClass: "card-header"
   }, [_vm._v("Example Component")]), _vm._v(" "), _c("div", {
     staticClass: "card-body"
-  }, [_vm._v("\n                    I'm an example component.\n                ")])])])])]);
+  }, [_vm._v("\n                    I'm an example compo\n                ")])])])])]);
 }];
 render._withStripped = true;
 
--- bktel/webpack.mix.js
+++ bktel/webpack.mix.js
@@ -13,4 +13,8 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .vue()
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .copy(
+        'node_modules/@fortawesome/fontawesome-free/webfonts',
+        'public/webfonts'
+    );
