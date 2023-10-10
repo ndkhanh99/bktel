0895eae1d4f85719df5ecede1c1ec71e992f72f9
oai2002 <oai.nguyen2002@hcmut.edu.vn>
2023-10-10 16:54:30 +0700

    feature/student-crud-nxoai task2done

---
+++ bktel/app/Models/Student.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'last_name',
+        'first_name',
+        'student_code',
+        'department',
+        'faculty',
+        'address',
+        'phone',
+        'note',
+    ];
+
+     public function user()
+    {
+         return $this->hasOne(User::class, 'student_id');
+     }
+}
--- bktel/routes/api.php
+++ bktel/routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\StudentController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+
+
+
+Route::resource('students', StudentController::class);
