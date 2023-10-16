0ea15d47dbadd78c20b33cd8e914136a21426da1
oai2002 <oai.nguyen2002@hcmut.edu.vn>
2023-10-16 15:08:03 +0700

    feature/FE01-theme-nxoai TASK3DONE

--- bktel/app/Http/Controllers/Auth/RegisterController.php
+++ bktel/app/Http/Controllers/Auth/RegisterController.php
@@ -51,8 +51,8 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users', 'ends_with:@hcmut.edu.vn'],
+            'password' => ['required', 'string', 'min:8', 'confirmed', 'regex:/^(?=.*[0-9])(?=.*[a-zA-Z]).{8,}$/'],
         ]);
     }
 
--- bktel/resources/views/auth/register.blade.php
+++ bktel/resources/views/auth/register.blade.php
@@ -9,21 +9,27 @@
                     <img src="images/img-02.png" alt="IMG" style="height: 316px;">
                 </div>
     
-                <form class="login100-form validate-form"  action="{{ route('login') }}">
+                <form class="login100-form validate-form" method="post"  action="{{ route('register') }}">
+                @csrf    
                     <span class="login100-form-title">
                         Register Account
                     </span>
     
                     <div class="wrap-input100 validate-input">
-                        <input class="input100" type="text" name="username" placeholder="{{ __('Username') }}"
+                        <input class="input100" value="{{old('name')}}" type="text" name="name" placeholder="{{ __('Username') }}"
                          required>
                         <span class="focus-input100"></span>
                         <span class="symbol-input100">
                             <i class="fa fa-user" aria-hidden="true"></i>
                         </span>
+                        @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
                     </div>
 
-                    <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+                    <div class="wrap-input100 validate-input" data-validate = "Valid email is required: xyz@hcmut.edu.vn">
                         <input class="input100" type="text" name="email" placeholder="{{ __('Email') }}"
                             value="{{ old('email') }}" required autocomplete="email">
                         <span class="focus-input100"></span>
@@ -38,7 +44,7 @@
                     </div>
     
                     <div class="wrap-input100 validate-input" data-validate = "Password is required">
-                        <input class="input100" type="password" name="pass" placeholder="{{ __('Password') }}"  class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                        <input class="input100" type="password" name="password" placeholder="{{ __('Password') }}"  class="form-control" id="password"  required autocomplete="current-password">
                         <span class="focus-input100"></span>
                         <span class="symbol-input100">
                             <i class="fa fa-lock" aria-hidden="true"></i>
@@ -51,11 +57,17 @@
                     </div>
     
                     <div class="wrap-input100 validate-input" data-validate = "Password is required">
-                        <input class="input100" type="password" name="confirmPass" placeholder="{{ __('Confirm Password') }}"  class="form-control" name="confirmPassword" required>
+                        <input class="input100" type="password" name="password_confirmation" placeholder="{{ __('Confirm Password') }}"  class="form-control" name="confirmPassword" id="password_confirmation"  required autocomplete="current-password">
+                        <span class="focus-input100"></span> 
                         <span class="focus-input100"></span>
                         <span class="symbol-input100">
                             <i class="fa fa-lock" aria-hidden="true"></i>
                         </span>
+                        @error('password_confirmation')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
                     </div>
     
                     <div class="container-login100-form-btn">
@@ -69,6 +81,16 @@
                             Back to Login <-
                         </a>
                     </div>
+
+                    @if ($errors->any())
+<div class="alert alert-danger">
+   <ul>
+    @foreach ($errors->all() as $error)
+      <li>{{ $error }}</li>
+    @endforeach
+    </ul>
+</div>
+@endif
                 </form>
             </div>
         </div>
