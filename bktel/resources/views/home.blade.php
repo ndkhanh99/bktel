40313b8d44e2c5790c12714abfc431d303184eec
QuangTruong2014907 <quangtruong6742@gmail.com>
2023-10-29 22:15:48 +0700

    feature/FE02-student-info-dqtruong Task5 fix

--- bktel/resources/views/home.blade.php
+++ bktel/resources/views/home.blade.php
@@ -1,5 +1,5 @@
 
-@extends('layouts.app')
+@extends('layouts.userstudent')
 @section('content')
 <body class="hold-transition sidebar-mini"> 
 
---
+++ bktel/resources/views/layouts/userstudent.blade.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    
+     <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+<!-- CSS CHO PAGE LOGIN OR RESGISTER -->
+<!--===============================================================================================-->	
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+
+  
+
+    <!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
+
+    <!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/font-awesome.min.css') }}">
+
+
+    <!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/animate.css') }}">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/hamburgers.min.css') }}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/select2.min.css') }}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/util.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/main.css') }}">
+<!--===============================================================================================-->
+
+
+<!-- CSS CHO PAGE HOME  -->
+
+  <!-- IonIcons -->
+ 
+
+  <link rel="stylesheet"  href="{{ asset('css/all.css') }}">
+  <link rel="stylesheet"  href="{{ asset('css/all.min.css') }}">
+
+  <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+
+
+  <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+
+  
+
+
+
+
+
+</head>
+<body>
+<div id="app">
+        
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+
+    <!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- Bootstrap -->
+	<!-- <script src="js/bootstrap.bundle.min.js"></script> -->
+	<!-- AdminLTE -->
+	<script src="js/adminlte.js"></script>
+
+	<!-- OPTIONAL SCRIPTS -->
+
+    <script src="js/Chart.min.js"></script>
+	<!-- AdminLTE for demo purposes -->
+	<script src="js/demo.js"></script>
+	<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+	<script src="js/dashboard3.js"></script>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
